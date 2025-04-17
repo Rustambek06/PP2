d137f47372f08714749b09dd83847f88f0ea0ee5
Rustambek <rusik.khanov06@gmail.com>
2025-04-17 11:47:23 +0500

    Lab11 updated

--- Lab11/PhoneBook/returnPattern.py
+++ Lab11/PhoneBook/returnPattern.py
@@ -1,35 +1,59 @@
-import csv
 import psycopg2
-from config import load_config
 from connect import get_connect
+from config import load_config
 
-#Search by Pattern
-def search_by_pattern(config):
+def create_function(config):
     conn = get_connect(config)
     cur = conn.cursor()
-    pattern = input("Enter search pattern (part of name or phone): ")
-    query = """
-        SELECT id, name, phone 
-        FROM PhoneBook 
-        WHERE name ILIKE %s OR phone ILIKE %s
-    """
-    like_pattern = f"%{pattern}%"
-    cur.execute(query, (like_pattern, like_pattern))
-    records = cur.fetchall()
-    if records:
-        print("\nMatching records:")
-        for record in records:
-            print(f"ID: {record[0]}, Name: {record[1]}, Phone: {record[2]}")
-    else:
-        print("No matching records found.")
+    cur.execute("""
+        CREATE OR REPLACE PROCEDURE search_users(p_pattern VARCHAR, INOUT ref refcursor)
+        LANGUAGE plpgsql
+        AS $$
+        BEGIN
+            OPEN ref FOR
+                SELECT *
+                FROM PhoneBook
+                WHERE name ILIKE '%' || p_pattern || '%'
+                OR phone ILIKE '%' || p_pattern || '%';
+        END;
+        $$;
+    """)
+    conn.commit()
+    print("Stored procedure 'search_users' created successfully.")
     cur.close()
     conn.close()
 
+
+def call_search_users(pattern, config):
+    conn = get_connect(config)
+    cur = conn.cursor()
+    
+    try:
+        cur.execute("BEGIN;")
+        cur.execute("CALL search_users(%s, 'search_result');", (pattern,))
+        cur.execute("FETCH ALL FROM search_result;")
+        rows = cur.fetchall()
+        
+        if rows:
+            for row in rows:
+                print(f"ID: {row[0]}, Name: {row[1]}, Phone: {row[2]}")
+        else:
+            print("No matching users found.")
+        
+        cur.execute("CLOSE search_result;")
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(f"Error: {error}")
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+
 def main():
     config = load_config()
-    print("Search by pattern")
-
-    search_by_pattern(config)
+    pattern = input("Enter pattern to search (part of name or phone): ")
+    create_function(config)
+    call_search_users(pattern, config)
 
 if __name__ == '__main__':
     main()
